d5ea906160b61b11591add758b518e51242be8c7
hyderali87 <34377320+hyderali87@users.noreply.github.com>
2024-03-17 12:28:38 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  AWS_ACCESS_KEY = "AKIA5FTZBNWO4MAZVINC"
-  AWS_SECRET_KEY = "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"
+  access_key = "AKIA5FTZBNWO4MAZVINC"
+  secret_key = "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"
 }
